5dc05d6120d610864eab51390b67e981084c87a4
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-02-06 14:29:28 +0100

    Added script for AIs to move freely.

---
+++ unity_project/PartyBlowout/Assets/Entities/Characters/Scripts/AI/AgentScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//TODO SYNC MOVEMENTS FOR ALL PLAYERS
+public class AgentScript : MonoBehaviour
+{
+    private Vector3 _target;
+    public float maxDist;
+    public NavMeshAgent agent;
+
+    /// <summary>
+    /// Generate a random position on the NavMesh
+    /// </summary>
+    /// <returns>A Vector3 position on the NavMesh</returns>
+    private Vector3 GetRandomPosOnNavMesh()
+    {
+        Vector2 circleRand = Random.insideUnitCircle * maxDist;
+        Vector3 randPos = agent.transform.position + new Vector3(circleRand.x, 0, circleRand.y);
+        NavMesh.SamplePosition(randPos, out NavMeshHit hit, maxDist, 1);
+        return hit.position;
+    }
+
+    void Update()
+    {
+        if (agent.isPathStale || !float.IsPositiveInfinity(agent.remainingDistance) &&
+            agent.pathStatus == NavMeshPathStatus.PathComplete &&
+            agent.remainingDistance <= agent.stoppingDistance)
+        {
+            _target = GetRandomPosOnNavMesh();
+        }
+        agent.SetDestination(_target);
+    }
+}

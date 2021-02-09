eb718fdd3b6e63f57da449410262de86c3516c8d
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-02-09 21:24:02 +0100

    Edited cryxDev scenes

---
+++ unity_project/PartyBlowout/Assets/Scenes/DevMaps/cryxDev/movementTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class movementTest : MonoBehaviour
+{
+    private CharacterController _CC;
+    private PhotonView _PV;
+
+    private void Start()
+    {
+        _CC = GetComponent<CharacterController>();
+        _PV = GetComponent<PhotonView>();
+    }
+
+    private void Movements()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _CC.Move(transform.right * Time.deltaTime * 5);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _CC.Move(-transform.right * Time.deltaTime * 5);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            _CC.Move(transform.forward * Time.deltaTime * 5);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _CC.Move(-transform.forward * Time.deltaTime * 5);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(_PV.IsMine)
+        {
+            Movements();
+        }
+    }
+}

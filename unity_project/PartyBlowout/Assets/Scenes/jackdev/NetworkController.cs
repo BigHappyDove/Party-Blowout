e4fe6aa2d333fa37cfc97cf67b48967a7e809bd6
jacques remy <jacques.remy@epita.fr>
2021-02-11 00:12:35 +0100

    Added NetworkController script (photon)

---
+++ unity_project/PartyBlowout/Assets/Scenes/jackdev/NetworkController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+//Documentation: https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+//Scripting API: https://doc-api.photonengine.com/en/pun/v2/index.html
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Photon master servers
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Now connected to the " + PhotonNetwork.CloudRegion + " server !");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

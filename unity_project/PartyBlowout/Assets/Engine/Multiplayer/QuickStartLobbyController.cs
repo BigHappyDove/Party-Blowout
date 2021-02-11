4342dfd3fe00cdf35ac7a6595cd654e86a898a52
clovis.lechien <clovis.lechien@epita.fr>
2021-02-11 14:58:57 +0100

    added some files which are awesome

---
+++ unity_project/PartyBlowout/Assets/Engine/Multiplayer/QuickStartLobbyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject quickStartButton;
+    [SerializeField] private GameObject quickCancelButton;
+    [SerializeField] private int RoomSize;
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        quickStartButton.SetActive(true);
+    }
+
+    public void QuickStart()
+    {
+        quickStartButton.SetActive(false);
+        quickCancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Quick Start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room.");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Creating a room..");
+        int randomRoomNumber = Random.Range(0, 10000); //Random room name
+        RoomOptions roomOps = new RoomOptions()
+            {IsVisible = true, IsOpen = true, MaxPlayers = (byte) RoomSize};
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+        Debug.Log(randomRoomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create a room.. trying again.");
+        CreateRoom();
+    }
+
+    public void QuickCancel()
+    {
+        quickCancelButton.SetActive(false);
+        quickStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ unity_project/PartyBlowout/Assets/Engine/Multiplayer/QuickStartRoomController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class QuickStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private int multiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room");
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting a game.");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+}

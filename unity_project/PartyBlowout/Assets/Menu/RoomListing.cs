4343337624a3f126500547d2b03091b8d6726c38
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-02-15 18:52:29 +0100

    Started to work on a player listing script

---
+++ unity_project/PartyBlowout/Assets/Menu/RoomListing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class RoomListing : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TextMeshProUGUI _textPlayerList;
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        UpdateListPlayers();
+        DebugTools.PrintOnGUI("OK1");
+    }
+
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        UpdateListPlayers();
+        DebugTools.PrintOnGUI("OK2");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DebugTools.PrintOnGUI("OK3");
+    }
+
+
+    public override void OnEnable() { UpdateListPlayers();}
+
+
+    private void UpdateListPlayers()
+    {
+        _textPlayerList.text = "";
+        foreach (KeyValuePair<int, Photon.Realtime.Player> val in PhotonNetwork.CurrentRoom.Players)
+            _textPlayerList.text += val.Value.NickName + "\n";
+    }
+
+}

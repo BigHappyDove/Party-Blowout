bcebb8fa0b66374dead2bc53a8e61025ed735897
clovis.lechien <clovis.lechien@epita.fr>
2021-04-17 15:15:16 +0200

    Working on checkpoints, might wanna SYNC THEM later

---
+++ unity_project/PartyBlowout/Assets/Entities/Misc/Car/CheckpointSingle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CheckpointSingle : MonoBehaviour
+{
+    private PhotonView _pv;
+    
+    private TrackCheckpoints _trackCheckpoints;
+    private MeshRenderer _meshRenderer;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        Hide();
+        _pv = GetComponent<PhotonView>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_pv.IsMine)
+        {
+            return;
+        }
+        if (other.TryGetComponent(out Player player))
+        {
+            _trackCheckpoints.PlayerThroughCheckpoint(this, other.transform);
+        }
+    }
+
+    public void SetTrackCheckpoints(TrackCheckpoints trackCheckpoints)
+    {
+        this._trackCheckpoints = trackCheckpoints;
+    }
+
+    public void Hide()
+    {
+        _meshRenderer.enabled = false;
+    }
+
+    public void Show()
+    {
+        _meshRenderer.enabled = true;
+    }
+}
---
+++ unity_project/PartyBlowout/Assets/Entities/Misc/Car/TrackCheckpoints.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+    private List<CheckpointSingle> _checkpointSingleList;
+    private List<int> nextCheckpointSingleIndexList;
+    private List<Transform> carTransformList;
+    private PhotonView _pv;
+
+    private void Start()
+    { 
+        _pv = GetComponent<PhotonView>();
+    }
+
+    private void Awake()
+    {
+        Transform checkpointsTransform = transform.Find("CHECKPOINTS");
+
+        _checkpointSingleList = new List<CheckpointSingle>();
+
+        foreach (Transform checkpointSingleTransform in checkpointsTransform)
+        {
+            CheckpointSingle checkpointSingle = checkpointsTransform.GetComponent<CheckpointSingle>();
+            checkpointSingle.SetTrackCheckpoints(this);
+            _checkpointSingleList.Add(checkpointSingle);
+        }
+
+        nextCheckpointSingleIndexList = new List<int>();
+        foreach (Transform carTransform in carTransformList)
+        {
+            nextCheckpointSingleIndexList.Add(0);
+        }
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle, Transform carTransform)
+    {
+        int nextCheckpointSingleIndex = nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)];
+        
+        CheckpointSingle correctcheckpointSingle = _checkpointSingleList[nextCheckpointSingleIndex];
+        
+        if (_checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex && _pv.IsMine)
+        {
+            nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)] = (nextCheckpointSingleIndex + 1) % _checkpointSingleList.Count;
+            
+            correctcheckpointSingle.Hide();
+            
+        }
+        else
+        {
+            //faire une fonction qui tp au checkpoint précédent
+            //et affiche sur le UI que c'est pas le bon checkpoint.
+            correctcheckpointSingle.Show();
+        }
+    }
+}

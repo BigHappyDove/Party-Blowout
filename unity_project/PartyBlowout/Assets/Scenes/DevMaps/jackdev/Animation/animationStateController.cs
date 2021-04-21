5a2b1a343c2dd393ae9756bb73523c57d653d313
jacques remy <jacques.remy@epita.fr>
2021-04-21 13:08:15 +0200

    Added player animation

--- unity_project/PartyBlowout/Assets/Scenes/DevMaps/jackdev/Animation/animationStateController.cs
+++ unity_project/PartyBlowout/Assets/Scenes/DevMaps/jackdev/Animation/animationStateController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class animationStateController : MonoBehaviour
@@ -7,12 +8,15 @@ public class animationStateController : MonoBehaviour
     Animator animator;
     int isWalkingHash;
     int isRunningHash;
+    
+    private PhotonView _photonView;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        
+        _photonView = GetComponent<PhotonView>();
+
         // Increase performance
         //isWalkingHash = Animator.StringToHash("isWalking");
         //isWalkingHash = Animator.StringToHash("isRunning");
@@ -21,6 +25,9 @@ public class animationStateController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!_photonView.IsMine)
+            return;
+        
         bool isRunning = animator.GetBool("isRunning");
         bool isWalking = animator.GetBool("isWalking");
         bool forwardPressed = Input.GetKey("z");

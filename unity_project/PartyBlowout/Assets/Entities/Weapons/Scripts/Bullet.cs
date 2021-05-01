88f7e64a6e0e52281efb5dbdf8ef2c66486dc1de
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-05-01 18:06:36 +0200

    Auto-destruct after 5secs

--- unity_project/PartyBlowout/Assets/Entities/Weapons/Scripts/Bullet.cs
+++ unity_project/PartyBlowout/Assets/Entities/Weapons/Scripts/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour, IPunInstantiateMagicCallback
 {
     private Rigidbody _rb;
     private PhotonView _pv;
-    private float time;
+    private float timeAlive = 5;
 
     private void Awake()
     {
@@ -16,8 +16,16 @@ public class Bullet : MonoBehaviour, IPunInstantiateMagicCallback
         _pv = GetComponent<PhotonView>();
     }
 
+    private void FixedUpdate()
+    {
+        if(!_pv.IsMine) return;
+        timeAlive -= Time.fixedDeltaTime;
+        if(timeAlive <= 0) PhotonNetwork.Destroy(gameObject);
+    }
+
     public void OnPhotonInstantiate(PhotonMessageInfo info)
     {
+        if(!_pv.IsMine) return;
         object[] args = info.photonView.InstantiationData;
         if (args[0] is Vector3 v && args[1] is float f)
         {
@@ -25,5 +33,9 @@ public class Bullet : MonoBehaviour, IPunInstantiateMagicCallback
         }
     }
 
-    private void OnCollisionEnter(Collision other) => PhotonNetwork.Destroy(gameObject);
+    private void OnCollisionEnter(Collision other)
+    {
+        if(!_pv.IsMine) return;
+        PhotonNetwork.Destroy(gameObject);
+    }
 }

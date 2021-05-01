9578070e207dacc85d45b1c2adfd7b79dcdcab2d
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-05-01 18:06:56 +0200

    Fixed issues

--- unity_project/PartyBlowout/Assets/Entities/Weapons/Scripts/WeaponBase.cs
+++ unity_project/PartyBlowout/Assets/Entities/Weapons/Scripts/WeaponBase.cs
@@ -131,6 +131,7 @@ public class WeaponBase : MonoBehaviour
 
     void ShootBullet()
     {
+        if(!PV.IsMine) return;
         //TODO: TEMPORARY VALUES. NEED TO BE SERIALIZED
         float forceBullet = 15;
         PhotonNetwork.Instantiate("Entities/Weapons/Bullet", bulletSpawnerPos.position, transform.rotation, 0,

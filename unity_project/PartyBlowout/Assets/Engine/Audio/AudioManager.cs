f948c042d2e5d05fa3c71465b2c1719196aa4925
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-05-02 19:26:01 +0200

    Moved files + Audio synced

---
+++ unity_project/PartyBlowout/Assets/Engine/Audio/AudioManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Serializable]
+    public struct Struct_sounds
+    {
+        public Sound sound;
+        public float maxDistance;
+    }
+
+    public Struct_sounds[] sounds;
+    private PhotonView _pv;
+    void Awake()
+    {
+        _pv = GetComponent<PhotonView>();
+        foreach (Struct_sounds s in sounds)
+        {
+            s.sound.source = gameObject.AddComponent<AudioSource>();
+            s.sound.source.clip = s.sound.clip;
+            s.sound.source.volume = s.sound.volume;
+            s.sound.source.pitch = s.sound.pitch == 0 ? 1 : s.sound.pitch;
+            s.sound.source.loop = s.sound.loop;
+            s.sound.source.maxDistance = s.maxDistance;
+            s.sound.source.minDistance = 1;
+            s.sound.source.spatialBlend = 1; // = 3D mode
+            s.sound.source.rolloffMode = AudioRolloffMode.Logarithmic;
+            s.sound.source.spatialize = true;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (Struct_sounds s in sounds)
+            s.sound.source.Stop();
+    }
+
+    private Struct_sounds? FindSound(string name)
+    {
+        Struct_sounds sound = Array.Find(sounds, s => s.sound.name == name);
+        if (sound.sound == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return null;
+        }
+        return sound;
+    }
+    public void Play(string name)
+    {
+        if(_pv == null || !_pv.IsMine) return;
+        _pv.RPC("RPC_Play", RpcTarget.All, name);
+    }
+
+    public void Stop(string name)
+    {
+        if(_pv == null || !_pv.IsMine) return;
+        _pv.RPC("RPC_Stop", RpcTarget.All, name);
+    }
+
+    [PunRPC]
+    public void RPC_Play(string name)
+    {
+        DebugTools.PrintOnGUI("Played on pos " + transform.position);
+        FindSound(name)?.sound.source.Play();
+    }
+
+    [PunRPC]
+    public void RPC_Stop(string name) => FindSound(name)?.sound.source.Stop();
+
+    // public void Pitch(string name, float value) => FindSound(name)?.sound.pitch = value;
+}
---
+++ unity_project/PartyBlowout/Assets/Entities/Characters/Scripts/Player/animationStateController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    private PhotonView _photonView;
+    private AudioManager _audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        _photonView = GetComponent<PhotonView>();
+        _audioManager = GetComponentInParent<AudioManager>();
+
+        // Increase performance
+        //isWalkingHash = Animator.StringToHash("isWalking");
+        //isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_photonView.IsMine)
+            return;
+
+        bool isRunning = animator.GetBool("isRunning");
+        bool isWalking = animator.GetBool("isWalking");
+        bool isWalkingBack = animator.GetBool("isWalkingBack");
+        bool forwardPressed = Input.GetKey("z");
+        bool backwardsPressed = Input.GetKey("s");
+        bool runPressed = Input.GetKey("left shift");
+
+        switch (isWalking)
+        {
+            case false when forwardPressed:
+                animator.SetBool("isWalking", true);
+                _audioManager.Play("Walk");
+                break;
+            case true when !forwardPressed:
+                animator.SetBool("isWalking", false);
+                _audioManager.Stop("Walk");
+                break;
+        }
+
+        switch (isWalkingBack)
+        {
+            case false when backwardsPressed:
+                animator.SetBool("isWalkingBack", true);
+                _audioManager.Play("Walk");
+                break;
+            case true when !backwardsPressed:
+                animator.SetBool("isWalkingBack", false);
+                _audioManager.Stop("Walk");
+                break;
+        }
+
+        switch (isRunning)
+        {
+            case false when (runPressed && (forwardPressed || backwardsPressed)):
+                animator.SetBool("isRunning", true);
+                break;
+            case true when (!runPressed || (!forwardPressed || !backwardsPressed)):
+                animator.SetBool("isRunning", false);
+                break;
+        }
+    }
+}

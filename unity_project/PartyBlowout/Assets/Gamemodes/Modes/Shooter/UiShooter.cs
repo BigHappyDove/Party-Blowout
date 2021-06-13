4326b354bfdb40800866a52a78c965768c1b07af
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-06-13 19:10:20 +0200

    Added UI for shooter

---
+++ unity_project/PartyBlowout/Assets/Gamemodes/Modes/Shooter/UiShooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UiShooter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI blueUI;
+    [SerializeField] private TextMeshProUGUI redUI;
+    void Start()
+    {
+        if(!blueUI || !redUI)
+            DebugTools.PrintOnGUI("Missing UI component in UIShooter.cs", DebugTools.LogType.WARNING);
+    }
+    void Update()
+    {
+        if(blueUI != null)
+            blueUI.SetText("Red: " + Shooter.Score[1]);
+        if(redUI != null)
+            redUI.SetText("Blue: " + Shooter.Score[0]);
+    }
+}

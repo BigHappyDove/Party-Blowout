2dfbb76cc4bbb884198d76eef846887ed117c7c2
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-06-17 15:44:35 +0200

    Added security for menu

---
+++ unity_project/PartyBlowout/Assets/Menu/WaitConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class WaitConnection : MonoBehaviour
+{
+    public GameObject connectingText;
+    public GameObject playMenu;
+
+    private void OnEnable()
+    {
+        connectingText.SetActive(true);
+        playMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        ShowUIConnection();
+    }
+
+    void ShowUIConnection()
+    {
+        connectingText.SetActive(!PhotonNetwork.IsConnectedAndReady);
+        playMenu.SetActive(PhotonNetwork.IsConnectedAndReady);
+    }
+}

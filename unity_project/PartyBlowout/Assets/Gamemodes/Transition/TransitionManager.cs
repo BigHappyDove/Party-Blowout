0a69d0fb3aca1580edb1c7936b3a167cfc00a4fa
Titouan Guionneau <titouan.guionneau@epita.fr>
2021-06-19 14:57:44 +0200

    Fix issues + added transition videos

---
+++ unity_project/PartyBlowout/Assets/Gamemodes/Transition/TransitionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TransitionManager : MonoBehaviour
+{
+    private VideoPlayer[] _videoPlayers;
+    private AudioSource _audio;
+    void Awake()
+    {
+        _videoPlayers = GetComponentsInChildren<VideoPlayer>();
+        _audio = GetComponent<AudioSource>();
+        foreach (VideoPlayer vp in _videoPlayers) vp.gameObject.SetActive(false);
+    }
+
+    public void PlaySelected(Gamemode.CurrentGamemode selectedGm)
+    {
+        foreach (VideoPlayer vp in _videoPlayers)
+            if (vp.gameObject.name == selectedGm.ToString())
+                PlayVideo(vp);
+    }
+
+    private void PlayVideo(VideoPlayer vp)
+    {
+        vp.gameObject.SetActive(true);
+        vp.targetCamera = FindObjectOfType<Camera>();
+        vp.Play();
+        if(_audio)
+            _audio.PlayOneShot(_audio.clip);
+    }
+}

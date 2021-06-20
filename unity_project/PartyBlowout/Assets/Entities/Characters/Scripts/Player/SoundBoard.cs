78f74b4e4bc33efa44d479cdc1dafddba73b6ead
jacques remy <jacques.remy@epita.fr>
2021-06-20 17:55:03 +0200

    Added soundboard

---
+++ unity_project/PartyBlowout/Assets/Entities/Characters/Scripts/Player/SoundBoard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBoard : AliveEntity
+{
+    private string lastSound;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PV.IsMine) 
+            return;
+
+        if (Gamemode.CurGamemode == Gamemode.CurrentGamemode.GuessWho)
+        {
+            if (Input.GetKeyDown("p"))
+            {
+                _audioManager.Stop(lastSound);
+                _audioManager.Play("Nani");
+                lastSound = "Nani";
+            }
+            
+            if (Input.GetKeyDown("o"))
+            {
+                _audioManager.Stop(lastSound);
+                _audioManager.Play("Deja-vu");
+                lastSound = "Deja-vu";
+            }
+            
+            if (Input.GetKeyDown("i"))
+            {
+                _audioManager.Stop(lastSound);
+                _audioManager.Play("Hello");
+                lastSound = "Hello";
+            }
+            
+            if (Input.GetKeyDown("u"))
+            {
+                _audioManager.Stop(lastSound);
+                _audioManager.Play("French");
+                lastSound = "French";
+            }
+        }
+
+    }
+}
